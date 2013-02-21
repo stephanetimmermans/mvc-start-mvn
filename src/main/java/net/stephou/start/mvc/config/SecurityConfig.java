ea0481e69677cc9a5056a084d8848f319b18de3f
Stéphane Timmermans <stephou0104@gmail.com>
2013-02-21 01:41:33 +0100

    MVC-2 : Secure the app with Spring Security

---
+++ src/main/java/net/stephou/start/mvc/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package net.stephou.start.mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author Stéphane Timmermans
+ * @version 1.0
+ */
+@Configuration
+@ImportResource("classpath:net/stephou/start/mvc/config/security.xml")
+public class SecurityConfig {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+    	return NoOpPasswordEncoder.getInstance();
+	}
+    
+	@Bean
+	public TextEncryptor textEncryptor() {
+		return Encryptors.noOpText();
+	}
+
+}
